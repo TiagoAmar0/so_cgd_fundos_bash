56fb8af3bca0a5cbe75b2b9196ed76137cac25d7
Tiago Amaro <tiagoamaro@MBP-de-Tiago-2.lan>
2020-04-09 14:23:46 +0100

    Added command to extract info about CX_ACOES_EUROPA_SOC_RESP

--- fundos_CGD.sh
+++ fundos_CGD.sh
@@ -9,6 +9,12 @@ CX_ACOES_EMERGENTES="EMER"
 CX_ACOES_LIDERES_GLOBAIS="GLOB"
 CAIXA_REFORMA_ACTIVA="Reforma"
 
+DATA_ATUAL_YMD="$(date +"%Y%m%d")"
+DATA_ATUAL_Y_M_D="$(date +"%d-%m-%Y")"
+NOME_FICHEIRO="fundos_CGD_$DATA_ATUAL_YMD.html"
+
+CABECALHO_DATA=" data|$DATA_ATUAL_Y_M_D"
+CABECALHO="Fundo|data|data-1|12meses|24meses"
 
 # ---------- FUNÇÕES
 # Manual de Utilização do Comando
@@ -18,17 +24,32 @@ function ajuda(){
 
 # Descarrega a página 
 function descarregarPagina(){
-    # Data formatada em AnoMesDia
-    data="$(date +"%Y%m%d")"
-    nome_ficheiro="fundos_CGD_$data.html"
-    
     # Descarregar ficheiro para o caminho desejado (fundos_CGD_DATA.html)
-    $(wget https://www.cgd.pt/Particulares/Poupanca-Investimento/Fundos-de-Investimento/Pages/CotacoeseRendibilidades.aspx -O $nome_ficheiro)
+    $(wget https://www.cgd.pt/Particulares/Poupanca-Investimento/Fundos-de-Investimento/Pages/CotacoeseRendibilidades.aspx -O $NOME_FICHEIRO)
     
 }
 
 function lerFicheiro(){
-    echo "------- Ler ficheiro ------"
+    # Verifica se o ficheiro existe no diretório atual
+    if test -f "$NOME_FICHEIRO"; then
+        echo "$CABECALHO_DATA"
+        echo "$CABECALHO"
+
+        # Extrair informações "Cx Ações Europa Soc Resp"
+        echo "$CX_ACOES_EUROPA_SOC_RESP|$(grep -A7 "Cx Ações Europa Soc Resp" $NOME_FICHEIRO \
+        | tail -n 7 \
+        | tr -d " " \
+        | tr -d "\</td\>" \
+        | tr -d "[67]" \
+        | tr -d "€" \
+        | tr "," "." \
+        | sed -r '/^\s*$/d' \
+        | tr "\n" "\|" \
+        | tr -d "\r")"
+
+    else 
+        echo "$NOME_FICHEIRO não existe"
+    fi
 }
 
 
@@ -44,11 +65,18 @@ if (($# > 0)); then
     while [[ -n $1 ]]; do
         case "$1" in 
             -h)
+                # Mostrar texto de ajuda do comando
                 ajuda
                 exit
             ;;
             -i)
+                # Descarregar a página web para o ficheiro fundos_CGD_DATA.html
                 descarregarPagina
+            ;;
+            *)
+                # Mostrar erro pois o parâmetro inserido é inválido
+                echo "\"./fundos_CGD.sh $1\" Parâmetro inválido."
+                exit
             ;;
         esac
     shift
